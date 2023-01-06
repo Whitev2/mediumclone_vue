88ab29eb63767ff13a97085de9d9d97da6a35ad2
Whitev2 <WilliamHarris9584680@gmail.com>
2023-01-06 15:49:30 +0500

    Revert "init"
    This reverts commit eb545bfdef796790beeff0d399ae136cef8aad5a.

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,17 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import auth from '@/store/modules/auth'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
-  modules: {auth},
+  state: {
+  },
+  getters: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
 })
